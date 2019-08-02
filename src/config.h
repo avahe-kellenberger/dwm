4217a2cbed01578402b4eb2205a5e6d1b6163a8b
Avahe Kellenberger <avahe@protonmail.ch>
2019-08-02 16:34:06 -0400

    Increased gaps and removed unused command.

--- src/config.h
+++ src/config.h
@@ -5,16 +5,15 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 
 /* gaps */
-static const unsigned int gappih    = 32;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 32;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 32;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 32;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 64;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 64;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 64;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 64;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
 static const char normbgcolor[]     = "#1c1b19";
 static const char normbordercolor[] = "#1c1b19"; 
 static const char normfgcolor[]     = "#fce8c3"; 
@@ -77,8 +76,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "selfrestart.c"
@@ -86,7 +83,6 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -148,7 +144,6 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
