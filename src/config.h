311c79c24e82ea73820c449ace833698b36a6802
Avahe Kellenberger <avahe@protonmail.ch>
2019-08-27 22:10:40 -0400

    systray support in progress.

--- src/config.h
+++ src/config.h
@@ -4,6 +4,13 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 
+/* systray */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+
+
 /* gaps */
 static const unsigned int gappih    = 64;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 64;       /* vert inner gap between windows */
