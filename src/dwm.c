e1ba07bfd1fe0573a7c02a485ddbb65a4b8b3813
Avahe Kellenberger <avahe@protonmail.ch>
2019-09-09 16:39:01 -0400

    Added systray.
    -NOTE: It will not be transparent.

--- src/dwm.c
+++ src/dwm.c
@@ -2674,7 +2674,7 @@ updatesystray(void)
 	XMapSubwindows(dpy, systray->win);
 	/* redraw background */
 	XSetForeground(dpy, drw->gc, scheme[SchemeNorm][ColBg].pixel);
-	XFillRectangle(dpy, systray->win, drw->gc, 0, 0, w, bh);
+    XCreateGC(dpy, systray->win, 0, NULL);
 	XSync(dpy, False);
 }
  
