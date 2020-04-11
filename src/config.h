ad3c18fb1982cddbe211fc12f3784bf4b9b912bc
Avahe Kellenberger <avahe@protonmail.ch>
2020-04-10 23:50:31 -0400

    mod+shift+q to killclient

--- src/config.h
+++ src/config.h
@@ -74,7 +74,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,         {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,             {0} },
 	{ MODKEY,                       XK_Tab,    view,             {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,       {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,       {0} },
 	{ MODKEY,                       XK_t,      setlayout,        {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                       XK_space,  togglefloating,   {0} },
