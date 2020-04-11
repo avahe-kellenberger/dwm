d01c49ab8da205b6255f4484b4735b94c9aaf354
Avahe Kellenberger <avahe@protonmail.ch>
2020-04-11 00:29:07 -0400

    Removed unused 'pop' function.

--- src/dwm.c
+++ src/dwm.c
@@ -239,7 +239,6 @@ static void monocle(Monitor *m);
 static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
-static void pop(Client *);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
@@ -1489,15 +1488,6 @@ nexttiled(Client *c)
 {
 	for (; c && (c->isfloating || !ISVISIBLE(c)); c = c->next);
 	return c;
-}
-
-void
-pop(Client *c)
-{
-	detach(c);
-	attach(c);
-	focus(c);
-	arrange(c->mon);
 }
 
 void
